a838d4f8a4888fa8e49173254276a64cb1f56939
Ramiz Polic <rpolic@cisco.com>
2025-02-19 12:31:35 +0100

    fix: add base database
    Signed-off-by: Ramiz Polic <rpolic@cisco.com>

---
+++ server/database/agent_table.go
@@ -0,0 +1,82 @@
+package database
+
+import (
+	"context"
+	coretypes "github.com/agntcy/dir/api/core/v1alpha1"
+	ds "github.com/dep2p/libp2p/datastore"
+	"github.com/dep2p/libp2p/datastore/query"
+	"gorm.io/gorm"
+)
+
+const agentTableName = "agents"
+
+// AgentTable handles all database operations for agent data models.
+//
+// Following notes are true:
+//   - The table is served via SQL. The constructor only needs the database instance
+//     and to reserve a specific table name (use agentTableName).
+//   - Keys are always constructed with the schema: /<namespace>/<agent-digest>.
+//     Use AgentCID function on the received key in db ops to extract agent digest/CID.
+//   - Value that is being exchanged (read/written/queried) is of type core.ObjectMeta.
+//     We can create our own database object as well.
+//   - We can use the StoreService to perform O(1) operations in Get, GetSize, Has
+//     for optimization,but this can be done some other time.
+type agentTable struct{}
+
+func New(db *gorm.DB) (ds.Datastore, error) {
+	return &agentTable{}, nil
+}
+
+func (s *agentTable) Get(ctx context.Context, key ds.Key) (value []byte, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *agentTable) Has(ctx context.Context, key ds.Key) (exists bool, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+// GetSize we can fake return back here
+func (s *agentTable) GetSize(ctx context.Context, key ds.Key) (size int, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+// Query dont implement now
+func (s *agentTable) Query(ctx context.Context, q query.Query) (query.Results, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *agentTable) Put(ctx context.Context, key ds.Key, value []byte) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *agentTable) Delete(ctx context.Context, key ds.Key) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+// Sync dont implement now
+func (s *agentTable) Sync(ctx context.Context, prefix ds.Key) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+// Close should only close the actual table, not the database itself
+func (s *agentTable) Close() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+// AgentCID extracts the agent digest (UID) from the key.
+// For example, for key=/<namespace>/<agent-digest>, we return digest=<agent-digest>
+func AgentCID(key ds.Key) (*coretypes.Digest, error) {
+	var digest coretypes.Digest
+	if err := digest.Decode(key.Name()); err != nil {
+		return nil, err
+	}
+	return &digest, nil
+}
--- server/go.mod
+++ server/go.mod
@@ -7,6 +7,7 @@ replace github.com/agntcy/dir/api => ../api
 require (
 	github.com/Portshift/go-utils v0.0.0-20220421083203-89265d8a6487
 	github.com/agntcy/dir/api v0.1.3
+	github.com/dep2p/libp2p v0.0.6
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/opencontainers/go-digest v1.0.0
 	github.com/opencontainers/image-spec v1.1.0
@@ -16,15 +17,20 @@ require (
 	github.com/stretchr/testify v1.10.0
 	google.golang.org/grpc v1.69.4
 	google.golang.org/protobuf v1.36.3
+	gorm.io/gorm v1.25.12
 	oras.land/oras-go/v2 v2.5.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jbenet/goprocess v0.1.4 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
@@ -37,8 +43,8 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67 // indirect
-	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/exp v0.0.0-20250106191152-7588d65b2ba8 // indirect
+	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
