1a59cceabeaf2844a5f6fe6bfc43c0c70450f6e4
Ramiz Polic <rpolic@cisco.com>
2025-03-24 13:26:23 +0100

    resolve rebase
    Signed-off-by: Ramiz Polic <rpolic@cisco.com>

--- client/routing.go
+++ client/routing.go
@@ -16,7 +16,6 @@ import (
 func (c *Client) Publish(ctx context.Context, ref *coretypes.ObjectRef) error {
 	_, err := c.RoutingServiceClient.Publish(ctx, &routingtypes.PublishRequest{
 		Record: ref,
-		Peer:   nil, // TODO check if required in client
 	})
 	if err != nil {
 		return fmt.Errorf("failed to publish object: %w", err)
@@ -27,7 +26,7 @@ func (c *Client) Publish(ctx context.Context, ref *coretypes.ObjectRef) error {
 
 func (c *Client) List(ctx context.Context, query string) ([]*coretypes.ObjectRef, error) {
 	stream, err := c.RoutingServiceClient.List(ctx, &routingtypes.ListRequest{
-		Query: query,
+		// Query: query,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to create pull stream: %w", err)
