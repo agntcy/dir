7a37bd6cec45c0e92dd28192b6e1e08aa0c2ebea
Tagscherer Ádám <adam.tagscherer@gmail.com>
2025-03-25 16:59:06 +0100

    feat: add list agents CLI command

--- cli/cmd/list/agents/agents.go
+++ cli/cmd/list/agents/agents.go
@@ -4,7 +4,13 @@
 package agents
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+
+	routetypes "github.com/agntcy/dir/api/routing/v1alpha1"
 	"github.com/agntcy/dir/cli/presenter"
+	"github.com/agntcy/dir/cli/util"
 	"github.com/spf13/cobra"
 )
 
@@ -23,7 +29,34 @@ var Command = &cobra.Command{
 }
 
 func runCommand(cmd *cobra.Command) error {
-	presenter.Printf(cmd, "hello agents command, query: %s\n", opts.Query)
+	// Get the client from the context.
+	c, ok := util.GetClientFromContext(cmd.Context())
+	if !ok {
+		return errors.New("failed to get client from context")
+	}
+
+	isLocal, _ := cmd.Flags().GetBool("local")
+
+	maxHops := uint32(10) //nolint:mnd
+
+	items, err := c.List(cmd.Context(), &routetypes.ListRequest{
+		Labels:  strings.Split(opts.Query, ","),
+		MaxHops: &maxHops,
+		Local:   &isLocal,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to list peers: %w", err)
+	}
+
+	for item := range items {
+		presenter.Printf(cmd,
+			"Peer: %v | Labels: %v | Annotations: %v | Digest: %v\n",
+			item.GetPeer().GetId(),
+			item.GetLabels(),
+			item.GetRecord().GetAnnotations(),
+			item.GetRecord().GetDigest(),
+		)
+	}
 
 	return nil
 }
