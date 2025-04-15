a0739dde55d0ae99723370e27d3124a9f8e9a4b6
Tagscherer Ádám <adam.tagscherer@gmail.com>
2025-04-15 21:19:48 +0200

    feat(client): add python client sdk
    Signed-off-by: Tagscherer Ádám <adam.tagscherer@gmail.com>

--- client/python/client/client.py
+++ client/python/client/client.py
@@ -6,6 +6,9 @@ import io
 import logging
 import grpc
 from typing import Optional, List, Iterator, BinaryIO, Tuple
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../api/python")))
 
 from core.v1alpha1 import object_pb2
 from routing.v1alpha1 import routing_service_pb2
