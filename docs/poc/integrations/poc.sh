b8a761d4bc767481a56117e1ed9a883a5be3c8ed
Ramiz Polic <rpolic@cisco.com>
2025-06-17 13:19:57 +0200

    fix: example poc shell script
    Signed-off-by: Ramiz Polic <rpolic@cisco.com>

--- docs/poc/integrations/poc.sh
+++ docs/poc/integrations/poc.sh
@@ -2,36 +2,22 @@
 
 ######################################################################
 ## Example using Directory as a way to integrate Agentic workflows
-## with **Continue VSCode extension** natively
+## with **VSCode Copilot** and **Continue VSCode extension** natively
 ######################################################################
 
-## Get the path of project root directory and the .vscode directory
+## Get the path of script directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-VSCODE_DIR=$SCRIPT_DIR/../../../.vscode
 
-######################################################################
-#################################### Hub/Directory flow
+#### Hub/Directory flow
 ## Push record to ADS
-# DIGEST=$(dirctl push record.json)
+# DIGEST=$(dirctl push $SCRIPT_DIR/record.json)
 
 ## Pull record from ADS
-# dirctl pull $DIGEST > record.json
+# dirctl pull $DIGEST > $SCRIPT_DIR/record.json
 
-## Search record from ADS
-# dirctl search --query "name:record.json" --output json > record.json
-
-######################################################################
-#################################### MCP Support
-## Extract MCP information from the record
-cat record.json | jq '.extensions[] | select(.name == "schema.oasf.agntcy.org/features/runtime/mcp") | .data' > mcp.json
-
-## Add the information to VSCode
-mkdir -p $VSCODE_DIR
-cp -f mcp.json $VSCODE_DIR/mcp.json
-
-## Cleanup
-rm -rf mcp.json
-
-######################################################################
-#################################### Model Support
-
+#### Integration support
+# Requirements: Python3, venv, pyyaml
+python3 $SCRIPT_DIR/poc.py \
+        -record=$SCRIPT_DIR/record.json \
+        -vscode_path=$SCRIPT_DIR/../../../.vscode \
+        -continue_path=$SCRIPT_DIR/../../../.continue/assistants
