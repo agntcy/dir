bcaee3b370420d9ac3c4a8b3d5549001048837b0
Tagscherer Ádám <adam.tagscherer@gmail.com>
2025-06-20 09:23:58 +0200

    feat: extension, locator queries revamp
    Signed-off-by: Tagscherer Ádám <adam.tagscherer@gmail.com>

--- cli/cmd/search/search.go
+++ cli/cmd/search/search.go
@@ -30,11 +30,9 @@ Usage examples:
 		--query "agent-version:v1.0.0" \
 		--query "skill-id:10201" \
 		--query "skill-name:Text Completion" \
-		--query "locator-type:docker-image" \
-		--query "locator-url:https://uri-to-locator" \
-		--query "extension-name:my-custom-extension-name" \
-		--query "extension-version:v1.0.0"
-		
+		--query "locator:docker-image:https://example.com/docker-image" \
+		--query "extension:my-custom-extension-name:v1.0.0" 
+
 `,
 	RunE: func(cmd *cobra.Command, _ []string) error {
 		return runCommand(cmd)
@@ -59,6 +57,18 @@ func runCommand(cmd *cobra.Command) error {
 		}
 
 		query := strings.SplitN(q, ":", 2) //nolint:mnd
+
+		if len(query) != 2 { //nolint:mnd
+			return fmt.Errorf("invalid query format: %s, expected format is 'type:value'", q)
+		}
+
+		if _, ok := searchtypesv1alpha2.RecordQueryType_value[query[0]]; !ok {
+			return fmt.Errorf("invalid query type: %s, valid types are: %v", query[0], searchtypesv1alpha2.ValidQueryTypes)
+		}
+
+		if query[1] == "" {
+			return fmt.Errorf("query value cannot be empty for type: %s", query[0])
+		}
 
 		queries = append(queries, &searchtypesv1alpha2.RecordQuery{
 			Type:  searchtypesv1alpha2.RecordQueryType(searchtypesv1alpha2.RecordQueryType_value[query[0]]),
