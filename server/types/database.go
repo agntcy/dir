ef835f3de88e82da7d424f91fc69a510b411a101
Catarina Paralta <clouropa@cisco.com>
2025-07-02 16:42:34 +0100

    feat: add sync to database
    Signed-off-by: Catarina Paralta <clouropa@cisco.com>

--- server/types/database.go
+++ server/types/database.go
@@ -4,9 +4,31 @@
 package types
 
 type DatabaseAPI interface {
+	SearchDatabaseAPI
+	SyncDatabaseAPI
+}
+
+type SearchDatabaseAPI interface {
 	// AddRecord adds a new agent record to the database.
 	AddRecord(record RecordObject) error
 
 	// GetRecords retrieves agent records based on the provided RecordFilters.
 	GetRecords(opts ...FilterOption) ([]RecordObject, error)
+}
+
+type SyncDatabaseAPI interface {
+	// CreateSync creates a new sync object in the database.
+	CreateSync(syncObject SyncObject) (string, error)
+
+	// GetSyncByID retrieves a sync object by its ID.
+	GetSyncByID(syncID string) (SyncObject, error)
+
+	// GetSyncs retrieves all sync objects.
+	GetSyncs() ([]SyncObject, error)
+
+	// UpdateSync updates an existing sync object in the database.
+	UpdateSync(syncObject SyncObject) error
+
+	// DeleteSync deletes a sync object by its ID.
+	DeleteSync(syncID string) error
 }
