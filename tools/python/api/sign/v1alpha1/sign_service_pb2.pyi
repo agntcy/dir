2cd0dc4476182d16f6a01cdb155ed908681803b5
Árpád Csepi <csepi.arpad@outlook.com>
2025-07-03 15:23:26 +0200

    feat(api): add v1alpha1 support to sign grpc service
    Signed-off-by: Árpád Csepi <csepi.arpad@outlook.com>

---
+++ tools/python/api/sign/v1alpha1/sign_service_pb2.pyi
@@ -0,0 +1,75 @@
+from core.v1alpha1 import agent_pb2 as _agent_pb2
+from google.protobuf import empty_pb2 as _empty_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from collections.abc import Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class SignOIDCRequest(_message.Message):
+    __slots__ = ("agent", "id_token", "options")
+    class SignOpts(_message.Message):
+        __slots__ = ("fulcio_url", "rekor_url", "timestamp_url", "oidc_provider_url", "oidc_client_id", "oidc_token", "key")
+        FULCIO_URL_FIELD_NUMBER: _ClassVar[int]
+        REKOR_URL_FIELD_NUMBER: _ClassVar[int]
+        TIMESTAMP_URL_FIELD_NUMBER: _ClassVar[int]
+        OIDC_PROVIDER_URL_FIELD_NUMBER: _ClassVar[int]
+        OIDC_CLIENT_ID_FIELD_NUMBER: _ClassVar[int]
+        OIDC_TOKEN_FIELD_NUMBER: _ClassVar[int]
+        KEY_FIELD_NUMBER: _ClassVar[int]
+        fulcio_url: str
+        rekor_url: str
+        timestamp_url: str
+        oidc_provider_url: str
+        oidc_client_id: str
+        oidc_token: str
+        key: str
+        def __init__(self, fulcio_url: _Optional[str] = ..., rekor_url: _Optional[str] = ..., timestamp_url: _Optional[str] = ..., oidc_provider_url: _Optional[str] = ..., oidc_client_id: _Optional[str] = ..., oidc_token: _Optional[str] = ..., key: _Optional[str] = ...) -> None: ...
+    AGENT_FIELD_NUMBER: _ClassVar[int]
+    ID_TOKEN_FIELD_NUMBER: _ClassVar[int]
+    OPTIONS_FIELD_NUMBER: _ClassVar[int]
+    agent: _agent_pb2.Agent
+    id_token: str
+    options: SignOIDCRequest.SignOpts
+    def __init__(self, agent: _Optional[_Union[_agent_pb2.Agent, _Mapping]] = ..., id_token: _Optional[str] = ..., options: _Optional[_Union[SignOIDCRequest.SignOpts, _Mapping]] = ...) -> None: ...
+
+class SignWithKeyRequest(_message.Message):
+    __slots__ = ("agent", "private_key", "password")
+    AGENT_FIELD_NUMBER: _ClassVar[int]
+    PRIVATE_KEY_FIELD_NUMBER: _ClassVar[int]
+    PASSWORD_FIELD_NUMBER: _ClassVar[int]
+    agent: _agent_pb2.Agent
+    private_key: bytes
+    password: bytes
+    def __init__(self, agent: _Optional[_Union[_agent_pb2.Agent, _Mapping]] = ..., private_key: _Optional[bytes] = ..., password: _Optional[bytes] = ...) -> None: ...
+
+class SignOIDCResponse(_message.Message):
+    __slots__ = ("agent",)
+    AGENT_FIELD_NUMBER: _ClassVar[int]
+    agent: _agent_pb2.Agent
+    def __init__(self, agent: _Optional[_Union[_agent_pb2.Agent, _Mapping]] = ...) -> None: ...
+
+class SignWithKeyResponse(_message.Message):
+    __slots__ = ("agent",)
+    AGENT_FIELD_NUMBER: _ClassVar[int]
+    agent: _agent_pb2.Agent
+    def __init__(self, agent: _Optional[_Union[_agent_pb2.Agent, _Mapping]] = ...) -> None: ...
+
+class VerifyOIDCRequest(_message.Message):
+    __slots__ = ("agent", "expected_issuer", "expected_signer")
+    AGENT_FIELD_NUMBER: _ClassVar[int]
+    EXPECTED_ISSUER_FIELD_NUMBER: _ClassVar[int]
+    EXPECTED_SIGNER_FIELD_NUMBER: _ClassVar[int]
+    agent: _agent_pb2.Agent
+    expected_issuer: str
+    expected_signer: str
+    def __init__(self, agent: _Optional[_Union[_agent_pb2.Agent, _Mapping]] = ..., expected_issuer: _Optional[str] = ..., expected_signer: _Optional[str] = ...) -> None: ...
+
+class VerifyWithKeyRequest(_message.Message):
+    __slots__ = ("agent", "public_key")
+    AGENT_FIELD_NUMBER: _ClassVar[int]
+    PUBLIC_KEY_FIELD_NUMBER: _ClassVar[int]
+    agent: _agent_pb2.Agent
+    public_key: bytes
+    def __init__(self, agent: _Optional[_Union[_agent_pb2.Agent, _Mapping]] = ..., public_key: _Optional[bytes] = ...) -> None: ...
