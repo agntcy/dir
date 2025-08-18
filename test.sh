fa70077ab1e4588be5c03ed88b95d05631ad89ee
Ramiz Polic <rpolic@cisco.com>
2025-08-18 17:22:50 +0200

    feat: controllable deployment for federation
    Signed-off-by: Ramiz Polic <rpolic@cisco.com>

--- test.sh
+++ test.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-## VARIABLES
-DIR_TRUST_DOMAIN="dir-server.com"
-DIRCTL_TRUST_DOMAIN="dir-client.com"
-
-# Deploy everything
+# Deploy cluster
 task deploy:k8s:spire:cleanup
 task deploy:k8s:spire:cluster
+
+# Deploy server with SPIRE
+export SPIRE_NAMESPACE="server-spire"
+export SPIRE_CLUSTER_NAME="server-cluster"
+export SPIRE_TRUST_DOMAIN="server-cluster.com"
+export SPIRE_CLASS_NAME="server-spire"
+export SPIRE_CSI_DRIVER="server.spiffe.io"
 task deploy:k8s:spire:chart
 task deploy:k8s:spire:dir
+
+# Deploy client with SPIRE
+export SPIRE_NAMESPACE="client-spire"
+export SPIRE_CLUSTER_NAME="client-cluster"
+export SPIRE_TRUST_DOMAIN="client-cluster.com"
+export SPIRE_CLASS_NAME="client-spire"
+export SPIRE_CSI_DRIVER="client.spiffe.io"
+task deploy:k8s:spire:chart
 task deploy:k8s:spire:dirctl
 
 # Check logs
-wait 60
 task logs:k8s:spire:dir
 task logs:k8s:spire:dirctl
