14150d5dd393306a0bc9d9361f34342a6752dc27
Árpád Csepi <csepi.arpad@outlook.com>
2025-10-29 00:37:14 +0100

    fix(sdk/js): move api stubs
    Signed-off-by: Árpád Csepi <csepi.arpad@outlook.com>

--- sdk/dir-js/src/models/sign_v1.ts
+++ sdk/dir-js/src/models/sign_v1.ts
@@ -1,6 +1,6 @@
 // Copyright AGNTCY Contributors (https://github.com/agntcy)
 // SPDX-License-Identifier: Apache-2.0
 
-export * from 'api/agntcy/dir/sign/v1/sign_service_pb';
-export * from 'api/agntcy/dir/sign/v1/signature_pb';
-export * from 'api/agntcy/dir/sign/v1/public_key_pb';
+export * from 'models/agntcy/dir/sign/v1/sign_service_pb';
+export * from 'models/agntcy/dir/sign/v1/signature_pb';
+export * from 'models/agntcy/dir/sign/v1/public_key_pb';
--- sdk/dir-js/src/models/store_v1.ts
+++ sdk/dir-js/src/models/store_v1.ts
@@ -1,5 +1,5 @@
 // Copyright AGNTCY Contributors (https://github.com/agntcy)
 // SPDX-License-Identifier: Apache-2.0
 
-export * from 'api/agntcy/dir/store/v1/store_service_pb';
-export * from 'api/agntcy/dir/store/v1/sync_service_pb';
+export * from 'models/agntcy/dir/store/v1/store_service_pb';
+export * from 'models/agntcy/dir/store/v1/sync_service_pb';
