ae8e9e91792c273f63dad0d7b884860830805c77
Árpád Csepi <csepi.arpad@outlook.com>
2025-10-29 12:48:26 +0100

    refactor(sdk/js): use relative import
    Signed-off-by: Árpád Csepi <csepi.arpad@outlook.com>

--- sdk/dir-js/rollup.config.mjs
+++ sdk/dir-js/rollup.config.mjs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import json from '@rollup/plugin-json';
-import {readFileSync} from 'fs';
+import { readFileSync } from 'fs';
 import typescript from 'rollup-plugin-typescript2';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
