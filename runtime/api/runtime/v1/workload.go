563ffbb5866914414b2b694e23f82cfa155753f6
Ramiz Polic <rpolic@cisco.com>
2026-02-02 14:28:54 +0100

    chore: update buf
    Signed-off-by: Ramiz Polic <rpolic@cisco.com>

--- runtime/api/runtime/v1/workload.go
+++ runtime/api/runtime/v1/workload.go
@@ -5,26 +5,6 @@ package v1
 
 import "google.golang.org/protobuf/proto"
 
-func (x *Workload) PatchServices(services *WorkloadServices) {
-	if services == nil {
-		return
-	}
-
-	// Initialize if nil
-	if x.GetServices() == nil {
-		x.Services = &WorkloadServices{}
-	}
-
-	// Merge fields
-	if x.GetServices().GetA2A() == nil {
-		x.Services.A2A = services.GetA2A()
-	}
-
-	if x.GetServices().GetOasf() == nil {
-		x.Services.Oasf = services.GetOasf()
-	}
-}
-
 func (x *Workload) DeepCopy() *Workload {
 	if x == nil {
 		return nil
