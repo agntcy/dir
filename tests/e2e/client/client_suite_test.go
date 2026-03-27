60d3315774cba9552c9d99c9aca3739f0f9ac855
Ramiz Polic <ramiz.polic@hotmail.com>
2026-03-27 19:18:45 +0100

    fix(ci): switch to native kind networking for e2e tests
    Signed-off-by: Ramiz Polic <ramiz.polic@hotmail.com>

--- tests/e2e/client/client_suite_test.go
+++ tests/e2e/client/client_suite_test.go
@@ -4,11 +4,20 @@
 package client
 
 import (
+	"os"
 	"testing"
+	"time"
 
 	"github.com/agntcy/dir/tests/e2e/shared/config"
+	"github.com/agntcy/dir/tests/e2e/shared/utils"
 	ginkgo "github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
+)
+
+const (
+	defaultServerAddress = "localhost:8888"
+	readyPollInterval    = 2 * time.Second
+	readyTimeout         = 2 * time.Minute
 )
 
 var cfg *config.Config
@@ -27,3 +36,18 @@ func TestClientE2E(t *testing.T) {
 
 	ginkgo.RunSpecs(t, "Client Library E2E Test Suite")
 }
+
+var _ = ginkgo.BeforeSuite(func() {
+	addr := os.Getenv("DIRECTORY_CLIENT_SERVER_ADDRESS")
+	if addr == "" {
+		addr = defaultServerAddress
+	}
+
+	ginkgo.GinkgoWriter.Printf("Waiting for Directory apiserver at %s...\n", addr)
+	gomega.Eventually(utils.IsGrpcServerReady).
+		WithArguments(addr).
+		WithPolling(readyPollInterval).
+		WithTimeout(readyTimeout).
+		Should(gomega.Succeed())
+	ginkgo.GinkgoWriter.Printf("Directory apiserver is ready at %s\n", addr)
+})
