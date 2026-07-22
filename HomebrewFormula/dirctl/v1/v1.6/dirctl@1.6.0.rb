1de8fefd5e97a56f1694d84fe62ae855535e5a7b
Bendegúz Csirmaz <csirmazbendeguz@gmail.com>
2026-07-22 13:04:55 +0200

    chore(dirctl): add brew formula v1.6.0
    Signed-off-by: Bendegúz Csirmaz <csirmazbendeguz@gmail.com>

---
+++ HomebrewFormula/dirctl/v1/v1.6/dirctl@1.6.0.rb
@@ -0,0 +1,61 @@
+class DirctlAT160 < Formula
+  desc "Command-line interface for AGNTCY directory"
+  homepage "https://github.com/agntcy/dir"
+  version "v1.6.0"
+  license "Apache-2.0"
+  version_scheme 1
+
+  url "https://github.com/agntcy/dir/releases/download/#{version}"
+
+  on_macos do
+      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+          url "#{url}/dirctl-darwin-arm64"
+          sha256 "d5348d51f0716374f37a1584db2dd6410e9764bb74c8816ea1967bd47dbf8c39"
+
+          def install
+              bin.install "dirctl-darwin-arm64" => "dirctl"
+
+              system "chmod", "+x", bin/"dirctl"
+              generate_completions_from_executable(bin/"dirctl", "completion", shells: [:bash, :zsh, :fish])
+          end
+      end
+
+      if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+          url "#{url}/dirctl-darwin-amd64"
+          sha256 "e134840bb022a6162ba7b18f9dc825da5be03e79c1a8a05c8d5f8cc31d0d7736"
+
+          def install
+              bin.install "dirctl-darwin-amd64" => "dirctl"
+
+              system "chmod", "+x", bin/"dirctl"
+              generate_completions_from_executable(bin/"dirctl", "completion", shells: [:bash, :zsh, :fish])
+          end
+      end
+  end
+
+  on_linux do
+      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+          url "#{url}/dirctl-linux-arm64"
+          sha256 "f8768d96491fcc56bf53d263b762f5170bc41a426e8207fb0ba2fcc1a1c38a5a"
+
+          def install
+              bin.install "dirctl-linux-arm64" => "dirctl"
+
+              system "chmod", "+x", bin/"dirctl"
+              generate_completions_from_executable(bin/"dirctl", "completion", shells: [:bash, :zsh, :fish])
+          end
+      end
+
+      if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+          url "#{url}/dirctl-linux-amd64"
+          sha256 "49c147dbf1019adfb7b5ff343ac0e302fcf6f85a6fd4a1b49ffa0d78b1407065"
+
+          def install
+              bin.install "dirctl-linux-amd64" => "dirctl"
+
+              system "chmod", "+x", bin/"dirctl"
+              generate_completions_from_executable(bin/"dirctl", "completion", shells: [:bash, :zsh, :fish])
+          end
+      end
+  end
+end
